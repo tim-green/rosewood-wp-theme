e9c02f99b6d13af6546c4a7f38901a7868e38cce
Tim <github@timgreen.ws>
2021-10-17 16:45:14 +1100

    removed bootstrap and popper from cdn

--- functions.php
+++ functions.php
@@ -530,11 +530,11 @@ $theme_version = '1.0.0';
 
 		// 2. Scripts
 		wp_enqueue_script( 'jquery',  'https://code.jquery.com/jquery-3.6.0.min.js', false, $theme_version, true );
-		wp_enqueue_script( 'bootstrapjs', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', array( 'jquery' ), $theme_version, true );
+		//wp_enqueue_script( 'bootstrapjs', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', array( 'jquery' ), $theme_version, true );
 		wp_enqueue_script( 'mainjs', get_template_directory_uri() . '/assets/build/app.min.js', false, $theme_version, true );
 
-		wp_enqueue_script( 'bootstrapjsCDN', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', false, $theme_version, true );
-		wp_enqueue_script( 'popperCDN', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', false, $theme_version, true );
+		//wp_enqueue_script( 'bootstrapjsCDN', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', false, $theme_version, true );
+		//wp_enqueue_script( 'popperCDN', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', false, $theme_version, true );
 
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
