14484de32b9685ea97f3ad383bac2d20edf0af7d
Tim <github@timgreen.ws>
2021-10-18 13:23:38 +1100

    added have_posts

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
  */
 
 	get_header();
-
-	$page_id = get_option( 'page_for_posts' );
 ?>
+<div class="section-inner">
+
 <?php 
 
 $archive_title_elem 	= is_front_page() || ( is_home() && get_option( 'show_on_front' ) == 'posts' ) ? 'h2' : 'h1';
@@ -19,25 +19,6 @@ $archive_description 	= get_the_archive_description();
 if ( $archive_title || $archive_description ) : 
 	?>
 
-	<div class="row">
-		
-		<div class="col-md-12">
-			<?php
-				echo nl2br( apply_filters( 'the_content', get_post_field( 'post_content', $page_id ) ) ); // = echo content from Bloghome
-
-				edit_post_link( __( 'Edit', 'my-theme' ), '<span class="edit-link">', '</span>', $page_id );
-			?>
-		</div><!-- /.col -->
-
-		<div class="col-md-12">
-			<?php
-				get_template_part( 'archive', 'loop' );
-			?>
-		</div><!-- /.col -->
-		
-	</div><!-- /.row -->
-
-<?php get_footer(); ?>
 	<header class="page-header">
 
 		<?php if ( $archive_type ) : ?>
@@ -57,3 +38,60 @@ if ( $archive_title || $archive_description ) :
 		<?php if ( is_search() && ! have_posts() ) get_search_form(); ?>
 
 	</header><!-- .page-header -->
+
+	<?php 
+endif;
+
+if ( have_posts() ) :
+
+	$old_year = '1'; ?>
+
+	<div class="posts" id="posts">
+
+		<?php 
+		while ( have_posts() ) : the_post();
+
+			// Get the date of the current post
+			$current_year = get_the_date( 'Y' );
+
+			// If it's different than the old year, we need a new wrapper
+			if ( $current_year != $old_year ) :
+
+				// If it's a proper year, and not the one we added as a default before the loop, we have an open wrapper that needs closing
+				if ( 1 != $old_year ) {
+					echo '</ul><!-- /' . $old_year . '-->';
+				}
+
+				// Wrap the new year
+				echo '<ul>';
+
+				if ( ! is_date() ) :
+
+					?>
+
+					<li>
+						<h3 class="list-title"><a href="<?php echo esc_url( get_year_link( $current_year ) ); ?>"><?php echo $current_year; ?></a></h3>
+					</li>
+
+					<?php
+
+				endif;
+
+				// Update the old_year variable
+				$old_year = $current_year;
+
+			endif;
+
+			get_template_part( 'content', get_post_type() );
+
+		endwhile;
+		?>
+
+	</div><!-- .posts -->
+
+<?php endif; ?>
+
+</div><!-- .section-inner -->
+
+<?php get_template_part( 'pagination' );
+get_footer(); ?>
