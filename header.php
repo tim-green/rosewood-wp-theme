d40270f74e7951db4ee66b2615a6ff42bbbb6411
Tim <github@timgreen.ws>
2021-10-18 13:16:58 +1100

    added fontawesome script

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+	<!-- FA added -->
+	<script src="https://kit.fontawesome.com/c20cab8581.js" crossorigin="anonymous"></script>
 	<?php wp_head(); ?>
 </head>
 
