f3a0d6cd76c72bd580132315d0812a82bf3c78e5
Tim <github@timgreen.ws>
2021-10-18 13:42:49 +1100

    added markup for comments

---
+++ comments.php
@@ -0,0 +1,32 @@
+<?php if ( $comments ) : ?>
+
+<div class="comments">
+
+	<h3 class="comment-reply-title"><?php comments_number( __( 'No Comments', 'rosewood' ), __( 'One Comment', 'rosewood' ), __( '% Comments', 'rosewood' ) ); ?></h3>
+
+	<?php
+
+	wp_list_comments( array(
+		'callback' 		=> 'rosewood_comment',
+		'style'			=> 'div',
+	) );
+
+	if ( paginate_comments_links( 'echo=0' ) ) : ?>
+
+		<div class="comments-pagination pagination">
+			<?php paginate_comments_links(); ?>
+		</div>
+
+	<?php endif; ?>
+
+</div><!-- comments -->
+
+<?php endif; ?>
+
+<?php if ( comments_open() || pings_open() ) : ?>
+
+<?php 
+	comment_form( 'comment_notes_before=&comment_notes_after=' );
+?>
+
+<?php endif; ?>
