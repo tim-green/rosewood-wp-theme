43c7d7f913c3e4e99ee2c49e09cb59cff0e9ff70
Tim <github@timgreen.ws>
2021-10-20 17:53:34 +1100

    removed button tag

--- searchform.php
+++ searchform.php
@@ -7,5 +7,4 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="screen-reader-text" for="<?php echo $unique_id; ?>"><?php _e( 'Search for:', 'rosewood' ); ?></label>
 	<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php esc_attr_e( 'Enter your search query',  'rosewood' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" autocomplete="off" />
-	</button>
 </form>
