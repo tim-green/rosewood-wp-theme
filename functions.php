2782306f9ca876cb8d5cf931c2c84e4ccf86088b
Tim <github@timgreen.ws>
2026-03-16 12:17:38 +1100

    changed js and css references

--- functions.php
+++ functions.php
@@ -529,7 +529,7 @@ $theme_version = '1.0.1';
 		// 1. Styles
 		wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', false, $theme_version, 'all' );
 		// wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/css/bootstrap.min.css', false, $theme_version, 'all' );
-		wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/build/app.min.css', false, $theme_version, 'all' ); // main.scss: Compiled Framework source + custom styles
+		wp_enqueue_style( 'main', get_template_directory_uri() . '/build/app.min.css', false, $theme_version, 'all' ); // main.scss: Compiled Framework source + custom styles
 		
 		if ( is_rtl() ) {
 			wp_enqueue_style( 'rtl', get_template_directory_uri() . '/assets/css/rtl.min.css', false, $theme_version, 'all' );
@@ -538,7 +538,7 @@ $theme_version = '1.0.1';
 		// 2. Scripts
 		wp_enqueue_script( 'jquery',  'https://code.jquery.com/jquery-3.6.0.min.js', false, $theme_version, true );
 		wp_enqueue_script('masonry');
-		wp_enqueue_script( 'mainjs', get_template_directory_uri() . '/assets/build/app.min.js', false, $theme_version, true );
+		wp_enqueue_script( 'mainjs', get_template_directory_uri() . '/build/app.min.js', false, $theme_version, true );
 
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
